598c229dcd825da5fedec5914762a5151060f8d8
ylchong <cgynlai@gmail.com>
2021-04-27 22:37:03 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,56 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function BookList () {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book /><Book /><Book /><Book />
+      
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    
+    </article>
+  )
+}
+
+const Image = () => {
+  return (
+    <img className='isize' src="https://images-na.ssl-images-amazon.com/images/I/71UjpVwXWJL._AC_UL200_SR200,200_.jpg">
+    </img>
+  )
+}
+
+const Title = () => {
+  return (
+    <h2>How to Be Human: An Autistic Man's Guide to Life </h2>
+  )
+}
+
+const Author = () => {
+  return (
+    <h5>Jory Fleming and Lyric Winik</h5>
+  )
+}
+
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
